916c2ec87b30dd0645fefdc13fe1eab16a98f2c8
Beckylee <dr.oreo@gmail.com>
2017-06-23 11:18:03 -0400

    cleaning up code

--- browser/js/client/clientController.js
+++ browser/js/client/clientController.js
@@ -1,18 +1,13 @@
 app.controller('clientCtrl', ($scope, $state, products, $stateParams, $rootScope,orderServices) => {
   $scope.products = products;
   $scope.total = 0;
-  $scope.error = {
-    noItems: false
-  };
+  $scope.error = false;
   $scope.success = false;
 
   //initialize order settings
   $scope.resetForm = function(){
-    $scope.products.forEach(function(prod){
-      prod.order = '0';
-    });
-
-    $scope.error.noItems = false;
+    $scope.products.forEach(prod => prod.order = '0');
+    $scope.error = false;
   }
 
   $scope.resetForm();
@@ -24,28 +19,26 @@ app.controller('clientCtrl', ($scope, $state, products, $stateParams, $rootScope
   //every time the order changes, the running total should too
   $scope.updateTotal = function(){
     var total = 0;
-    $scope.products.forEach(function(prod){
-      total += prod.order*prod.unit_price;
-    });
 
+    $scope.products.forEach(prod => total += prod.order*prod.unit_price);
     $scope.total = total;
   }
 
   $scope.submit = function(){
-    $scope.error.noItems = false;
+    $scope.error = false;
+
     //make sure at least one item has been selected
     if($scope.total == 0){
-      $scope.error.noItems = true;
+      $scope.error = true;
       return;
     }
     var order = {
       amount: $scope.total
     };
 
-
     var order_items = [];
 
-    $scope.products.forEach(function(prod){
+    $scope.products.forEach(prod => {
       if(prod.order > 0){
         var temp = {
           sold_quantity: prod.order,
@@ -59,14 +52,10 @@ app.controller('clientCtrl', ($scope, $state, products, $stateParams, $rootScope
     //send order req
     orderServices.createOrder(order, order_items)
       .then(function(res){
-        console.log(res);
-        //if it worked, reset everything & show success message
+        //reset everything & show success message
         $scope.resetForm();
         $scope.success = true;
-
-      })
+      });
   }
-
-
 
 });
--- browser/js/mgmt/mgmtController.js
+++ browser/js/mgmt/mgmtController.js
@@ -2,29 +2,26 @@ app.controller('mgmtCtrl', ($scope, $state, products, $stateParams, $rootScope,o
   $scope.products = products;
   $scope.orders = orders.orders;
   $scope.orderItems = orders.items;
-
   $scope.newProduct = {};
-  $scope.errors = {
-    newMissing: false
-  }
-
+  $scope.error = false;
   $scope.success = false;
 
   $scope.fullOrders = function(){
+    //map out all orders & products
     $scope.products.forEach(prod => {
       var temp = _.where($scope.orderItems, {product_sku: prod._id});
       temp.map(t => t.name = prod.name);
     })
-    $scope.orders.map(or => {
-      or.items = _.where($scope.orderItems, {order_id: or._id});
-    });
+
+    $scope.orders.map(or => or.items = _.where($scope.orderItems, {order_id: or._id}));
   }
 
   $scope.fullOrders();
 
   $scope.saveNew = function(){
-    $scope.errors.newMissing = false;
+    $scope.error = false;
 
+    //make sure that all fields are present
     if($scope.newProduct.name && $scope.newProduct.quantity && $scope.newProduct.unit_price){
       productServices.createProduct($scope.newProduct)
         .then(res => {
@@ -33,27 +30,19 @@ app.controller('mgmtCtrl', ($scope, $state, products, $stateParams, $rootScope,o
           $scope.success = true;
         });
     }else{
-      console.log('uh');
-      $scope.errors.newMissing = true;
+      $scope.error = true;
       return
     }
   }
 
   $scope.deleteProduct = function(p){
     productServices.deleteProduct(p)
-      .then(res => {
-        //something
-        console.log('yeah');
-
-        $state.reload();
-      })
+      .then(res => $state.reload())
   }
 
   $scope.updateProd = function(p){
     productServices.updateProduct(p)
-      .then(res => {
-        p.edit = false;
-      });
+      .then(res => p.edit = false);
   };
 
 });
