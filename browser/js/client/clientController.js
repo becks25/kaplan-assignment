aa5ab0a36710f3768710c550daf93e68e02f1d58
Beckylee <dr.oreo@gmail.com>
2017-06-24 13:20:32 -0400

    added upload ability

--- browser/js/client/clientController.js
+++ browser/js/client/clientController.js
@@ -3,6 +3,9 @@ app.controller('clientCtrl', ($scope, $state, products, $stateParams, $rootScope
   $scope.total = 0;
   $scope.error = false;
   $scope.success = false;
+  $scope.uploadFile;
+
+  document.getElementById('orderUpload').addEventListener('change', $scope.upload, false);
 
   //initialize order settings
   $scope.resetForm = function(){
@@ -24,38 +27,85 @@ app.controller('clientCtrl', ($scope, $state, products, $stateParams, $rootScope
     $scope.total = total;
   }
 
+
   $scope.submit = function(){
     $scope.error = false;
-
-    //make sure at least one item has been selected
-    if($scope.total == 0){
-      $scope.error = true;
-      return;
-    }
-    var order = {
-      amount: $scope.total
-    };
+    var up = document.getElementById('orderUpload');
 
     var order_items = [];
+    var order = {};
 
-    $scope.products.forEach(prod => {
-      if(prod.order > 0){
-        var temp = {
-          sold_quantity: prod.order,
-          unit_price: prod.unit_price,
-          product_sku: prod._id
-        };
-        order_items.push(temp);
+    if(up.files[0]){
+      console.log($scope.uploadFile);
+      var reader = new FileReader();
+          reader.readAsText(up.files[0]);
+          console.log(reader);
+
+          reader.onload = function(event) {
+                var csvData = event.target.result;
+                var data = csvData.split(/\n/);
+                var titles = data[0].split(',');
+                var total = 0;
+
+                for(var i = 1; i< data.length; i++){
+                  var curr = data[i].split(',');
+
+                  var temp = {
+                    sold_quantity: curr[0]
+                  }
+
+                  //find out sku & unit_price
+                  var prod =  _.where($scope.products, {name: curr[1].toLowerCase()});
+
+                  if(prod[0]){
+                    temp.unit_price = prod[0].unit_price;
+                    temp.product_sku = prod[0]._id;
+                    $scope.total += temp.unit_price * temp.sold_quantity;
+                    order_items.push(temp);
+                  }
+                  //if item isn't found, it isn't added to the order
+
+                }
+
+                order.amount= $scope.total;
+                //send order req
+                orderServices.createOrder(order, order_items)
+                  .then(function(res){
+                    //reset everything & show success message
+                    $scope.resetForm();
+                    $scope.success = true;
+                  });
+
+            };
+
+    }else{
+      //make sure at least one item has been selected
+      if($scope.total == 0){
+        $scope.error = true;
+        return;
       }
-    });
 
-    //send order req
-    orderServices.createOrder(order, order_items)
-      .then(function(res){
-        //reset everything & show success message
-        $scope.resetForm();
-        $scope.success = true;
+      $scope.products.forEach(prod => {
+        if(prod.order > 0){
+          var temp = {
+            sold_quantity: prod.order,
+            unit_price: prod.unit_price,
+            product_sku: prod._id
+          };
+          order_items.push(temp);
+        }
       });
+
+      order.amount= $scope.total;
+      //send order req
+      orderServices.createOrder(order, order_items)
+        .then(function(res){
+          //reset everything & show success message
+          $scope.resetForm();
+          $scope.success = true;
+        });
+
+    }
   }
 
 });
